d1c8c9aa5ef9d813054094cad585a26e7d236d45
danieltybr <cd095959@gmail.com>
2024-06-16 20:36:47 -0300

    Delete the file at 'eruda.js'

--- eruda.js
+++
@@ -1,1 +0,0 @@
-window.location.href = "javascript:(function () { var script = document.createElement('script'); script.src='https://cdn.jsdelivr.net/npm/eruda'; document.body.append(script); script.onload = function () { eruda.init(); } })();"
